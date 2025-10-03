73dbec412562cc40deb39b99b2ca266d8e53e9ea
quysnt <hello.quynt@gmail.com>
2025-10-04 01:08:03 +0700

    up section our

--- index.php
+++ index.php
@@ -98,6 +98,138 @@
       </ul>
     </div>
   </section>
+  <section class="p-home__since">
+    <div class="l-container">
+      <div class="p-home__since--box01">
+        <div>
+          <p class="c-text__intro01">
+            Since 1996
+          </p>
+          <h2 class="c-title__02">
+            <span>A Trusted</span>
+            <span>Multi-Sector Corporation</span>
+          </h2>
+        </div>
+        <div class="p-home__since--box01-right">
+          <p class="c-text01">
+            Founded in 1996, Tram Anh Group operates across 6 industries with 15+ subsidiaries, delivering sustainable growth and long-term value.
+          </p>
+          <a href="#" class="c-btn__02">
+            Discover Our Story
+          </a>
+        </div>
+      </div>
+      <div class="p-home__since--box02">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-page/img-home-02.png" alt="img">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-page/img-home-03.png" alt="img">
+      </div>
+    </div>
+  </section>
+  <section class="p-home__our">
+    <div class="l-container small">
+      <div class="p-home__our--box01">
+        <p class="c-text__intro01">
+          Our Core Sectors
+        </p>
+        <h2 class="c-title__02 type-02">
+          <span>Business</span>
+          <span>Highlights</span>
+        </h2>
+      </div>
+      <div class="p-home__our--box02">
+        <div class="p-home__our--box03">
+          <div class="p-home__our--item first">
+            <div class="icons">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-page/icons/icon-our-01.svg" alt="icon-our-01">
+            </div>
+            <div class="conents">
+              <h3 class="title">
+                Lubricants & Chemicals
+              </h3>
+              <p class="desc">
+                Founding shareholder of AP Saigon Petro and ChemStation Asia, leaders in blending and distribution.
+              </p>
+              <a href="#" class="c-btn__03">Read More</a>
+            </div>
+          </div>
+          <div class="p-home__our--item-center">
+            <div class="p-home__our--item-main">
+              <div class="icons">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-page/icons/icon-automotive.svg" alt="icon-automotive">
+              </div>
+              <div class="conents">
+                <h3 class="title">
+                  Automotive & Mobility
+                </h3>
+                <p class="desc">
+                  Audi Vietnam, Ducati, Royal Enfield, Aprilia, Moto Guzzi, Piaggio, Vespa, and EV One charging solutions.
+                </p>
+                <a href="#" class="c-btn__01">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="p-home__our--item last">
+            <div class="icons">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-page/icons/icon-our-02.svg" alt="icon-our-01">
+            </div>
+            <div class="conents">
+              <h3 class="title">
+                Sports & Community
+              </h3>
+              <p class="desc">
+                Owner of Thang Long Warriors Basketball Team, its Academy, and modern sports complexes under OCenter.
+              </p>
+              <a href="#" class="c-btn__03">Read More</a>
+            </div>
+          </div>
+        </div>
+        <div class="p-home__our--box04">
+          <div class="p-home__our--item">
+            <div class="icons">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-page/icons/icon-our-03.svg" alt="icon-our-03">
+            </div>
+            <div class="conents">
+              <h3 class="title">
+                Yachting & Lifestyle
+              </h3>
+              <p class="desc">
+                Founding partner of Tam Son Yachting.
+              </p>
+              <a href="#" class="c-btn__03">Read More</a>
+            </div>
+          </div>
+          <div class="p-home__our--item">
+            <div class="icons">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-page/icons/icon-our-04.svg" alt="icon-our-04">
+            </div>
+            <div class="conents">
+              <h3 class="title">
+                Real Estate
+              </h3>
+              <p class="desc">
+                Urban and industrial property developments and investments.
+              </p>
+              <a href="#" class="c-btn__03">Read More</a>
+            </div>
+          </div>
+          <div class="p-home__our--item">
+            <div class="icons">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-page/icons/icon-our-05.svg" alt="icon-our-05">
+            </div>
+            <div class="conents">
+              <h3 class="title">
+                Retail & Luxury
+              </h3>
+              <p class="desc">
+                Exclusive distributor of Jacob & Co Vietnam.
+              </p>
+              <a href="#" class="c-btn__03">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </main>
 
 <?php get_footer(); ?>
